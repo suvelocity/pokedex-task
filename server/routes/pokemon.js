45dcf3f8b38773d83b4039fcb8892610a9ecad0d
Nivm200 <Nivm200@gmail.com>
2021-03-18 14:39:50 +0200

    first comit! :)
    Succeed with axios get request!

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const pokemon = Router();
-
-pokemon.get("/", (req, res) => {
-  res.send("Pokemon route");
+//localhost:3001/api/pokemon/
+pokemon.get("/:name", (req, res) => {
+  let name = req.params.name;
+  axios.get("https://pokeapi.co/api/v2/pokemon/" + name).then((response) => {
+    if (response.status === 200) {
+      console.log(response.data.name);
+    }
+  });
+  res.send(name);
 });
 
 module.exports = pokemon;
