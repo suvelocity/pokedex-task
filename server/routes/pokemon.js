9e6e7a1bd29808baa4b82946a84e3e1b8535ab64
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-03-18 14:36:44 +0200

    pokemon get route

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,9 +1,21 @@
 const { Router } = require("express");
+const axios = require("axios");
 
 const pokemon = Router();
 
-pokemon.get("/", (req, res) => {
-  res.send("Pokemon route");
+pokemon.get("/:name", (req, res) => {
+  let name = req.params.name;
+  axios
+    .get("https://pokeapi.co/api/v2/pokemon/" + name)
+    .then((response) => {
+      console.log(response.data.name);
+      res.send(name);
+    })
+    .catch((error) => {
+      let statusCode = error.response.status;
+      res.send("Error, status code: " + statusCode);
+      //Todo- move to a cool 404 page.
+    });
 });
 
 module.exports = pokemon;
