c2047b382c3e3742c265d1ddc307b97ad7f7f397
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-18 20:58:33 +0200

    finished collections

--- server/routes/collection.js
+++ server/routes/collection.js
@@ -1,12 +1,30 @@
-const mongoose = require("mongoose");
 const { Router } = require("express");
 
 const collection = Router();
 
-let pokemonCollection = [];
+let pokemonCollection = [
+  { id: 85, name: "charzard" },
+  { id: 4, name: "pika" },
+];
 
+//getting the full array
 collection.get("/", async (req, res) => {
-  res.send(collection);
+  res.send(pokemonCollection);
+});
+
+//adding a pokemon to the array
+collection.post("/catch", async (req, res) => {
+  const { body } = req;
+  pokemonCollection.push(body);
+  res.status(200).send("pokemon Collected!");
+});
+
+//deleting a pokemon out of an array
+collection.delete("/release/:id", async (req, res) => {
+  const { id } = req.body;
+  const index = pokemonCollection.findIndex((pokemon) => pokemon.id === id);
+  pokemonCollection.splice(index, 1);
+  res.send(pokemonCollection);
 });
 
 module.exports = collection;
