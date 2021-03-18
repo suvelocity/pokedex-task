489e10760699e23f2503d80beb0187b14908a114
SamuelGasasa <77382869+SamuelGasasa@users.noreply.github.com>
2021-03-18 15:39:53 +0200

    type route returns array with the pokemons names of that type

--- server/routes/type.js
+++ server/routes/type.js
@@ -1,9 +1,23 @@
 const { Router } = require("express");
+const axios = require("axios");
+const pokemon = require("./pokemon");
 
 const type = Router();
 
-type.get("/", (req, res) => {
-  res.send("type route");
+type.get("/:name", (req, res) => {
+  const { name } = req.params;
+  axios
+    .get(`https://pokeapi.co/api/v2/type/${name}`)
+    .then((response) => {
+      const pokemonNames = response.data.pokemon.map((list) => {
+        return list.pokemon.name;
+      });
+      console.log(pokemonNames);
+      res.send(pokemonNames);
+    })
+    .catch((e) => {
+      res.send("couldn't find the type you are looking for");
+    });
 });
 
 module.exports = type;
