4caa85d9eb976df54add8035674968f88a7dea60
Nivm200 <Nivm200@gmail.com>
2021-03-18 15:41:29 +0200

    Finished search by ID and name

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -9,7 +9,20 @@ pokemon.get("/:name", (req, res) => {
     .get("https://pokeapi.co/api/v2/pokemon/" + name)
     .then((response) => {
       console.log(response.data.name);
-      res.send(name);
+      let obj = {};
+      obj.name = response.data.name;
+      obj.id = response.data.id;
+      obj.height = response.data.height;
+      obj.weight = response.data.weight;
+      let types = response.data.types;
+      let typesName = [];
+      for (let i = 0; i < types.length; i++) {
+        typesName.push(types[i].type.name);
+      }
+      obj.types = typesName;
+      obj.front = response.data.sprites.front_default;
+      obj.back = response.data.sprites.back_default;
+      res.send(obj);
     })
     .catch((error) => {
       let statusCode = error.response.status;
