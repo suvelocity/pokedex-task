ac0db6718ef18e5d508427cc9748a23d0b2ec108
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-03-19 11:55:11 +0200

    get pokemon end point

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,9 +1,19 @@
 const { Router } = require("express");
+const {getPokemon} = require("../utils/pokeAPI")
 
 const pokemon = Router();
 
-pokemon.get("/", (req, res) => {
-  res.send("Pokemon route");
+pokemon.get("/:name", async (req, res, next) => {
+  try {
+    const pokemon = await getPokemon(req.params.name);
+  } catch (error) {
+    next(error)
+  }
+  res.send(pokemon);
+});
+
+pokemon.use((err, req, res, next) => {
+  if (err === "not found") return res.status(404).end();
 });
 
 module.exports = pokemon;
--- server/utils/pokeAPI.js
+++ server/utils/pokeAPI.js
@@ -2,3 +2,27 @@ const { default: axios } = require("axios");
 const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2";
 
 // You can use this file to write helper functions
+
+async function getPokemon(name) {
+    try {
+        const response = await axios.get(`${POKEAPI_BASE_URL}/pokemon/${name}`);
+        const pokemon = response.data;
+    } catch (error) {
+        console.log(error.message);
+        throw "not found";
+    }
+    const pokemonData = {
+        name: pokemon.name,
+        id: pokemon.id,
+        height: pokemon.height,
+        weight: pokemon.weight,
+        types: pokemon.types.map(element => element.type.name),
+        sprites: {
+            front: pokemon.sprites.front_default,
+            back: pokemon.sprites.back_default,
+        }
+    }
+    return pokemonData
+};
+
+module.exports = {getPokemon}
