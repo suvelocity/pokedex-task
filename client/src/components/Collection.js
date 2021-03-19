b5ec16c96fa5380ed1fe4cb821c34cd7cc3ef47b
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-19 13:46:48 +0200

    adding collection component

--- client/src/components/Collection.js
+++ client/src/components/Collection.js
@@ -2,5 +2,14 @@ import React from "react";
 import "../styles/Collection";
 
 export default function Collection({ collection }) {
-  return <div></div>;
+  return (
+    <div>
+      <h1> Collection</h1>
+      <ul className="pokemon-list">
+        {collection.map((pokemon) => (
+          <li>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
