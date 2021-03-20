b810d0a508c1a8354c2289bb0d5c6eeb1fdf5eb0
eliav-touitou <77244738+eliav-touitou@users.noreply.github.com>
2021-03-20 20:07:30 +0200

    images on both sides

--- client/src/components/CollectionItem.js
+++ client/src/components/CollectionItem.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function CollectionItem({ pokemonName }) {
-  return <div>{pokemonName.name}</div>;
+  return (
+    <div>
+      {pokemonName.name}
+      <img
+        src={pokemonName.sprites?.front}
+        onMouseOver={(e) => (e.currentTarget.src = pokemonName.sprites?.back)}
+        onMouseOut={(e) => (e.currentTarget.src = pokemonName.sprites?.front)}
+      />
+    </div>
+  );
 }
